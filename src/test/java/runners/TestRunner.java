5cad1685303457b022203b6425d5d4f2be112d2a
Poulomi0789 <50943118+Poulomi0789@users.noreply.github.com>
2026-02-10 18:00:38 +0530

    Delete src/test/java/runners/TestRunner.java

--- src/test/java/runners/TestRunner.java
+++
@@ -1,9 +0,0 @@
-
-import com.intuit.karate.junit5.Karate;
-public class TestRunner {
-    @Karate.Test
-    Karate runAll() {
-        return Karate.run("classpath:features")
-                .parallel(5);
-    }
-}
