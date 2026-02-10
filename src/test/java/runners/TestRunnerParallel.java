69c1eae0ad870d3d721013f24a6dccac06910881
Poulomi0789 <50943118+Poulomi0789@users.noreply.github.com>
2026-02-10 17:54:59 +0530

    Update TestRunnerParallel.java

--- src/test/java/runners/TestRunnerParallel.java
+++ src/test/java/runners/TestRunnerParallel.java
@@ -1,4 +1,21 @@
 package runners;
 
-public class SmokeRunner {
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import io.qameta.allure.karate.AllureKarate;
+
+public class TestRunnerParallel {
+
+    @Test
+    void testParallel() {
+        // List all feature files you want to run in parallel
+        Results results = Runner.path("classpath:features")
+                .hook(new AllureKarate())
+                .outputCucumberJson(true)
+                .parallel(4); // 🔥 Runs ALL features in parallel
+
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
 }
